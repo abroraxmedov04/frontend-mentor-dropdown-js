e05892921ea2164c928c399e902d59e54a2fbe3d
abroraxmedov04 <abrordevaxmedov@gmail.com>
2024-04-04 03:32:40 +0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function dropDownFunctionality(e) {
+  if (!e.target.matches(".card__btn")) return;
+  e.target.classList.toggle("card__icon-close");
+  const card = e.target.closest(".card");
+  const cardBody = card.querySelector(".card__body");
+  cardBody.classList.toggle("hide");
+}
+
+document.addEventListener("click", dropDownFunctionality);
